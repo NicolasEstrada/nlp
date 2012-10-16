c87dc96fd33c0b98a42047f542a424c2d337df6e
Matias Estrada <matiasestrada.i@gmail.com>
2012-10-16 00:36:34 -0300

    Added Abstract support and insertion to MySql. TODO: Check the text encoding. TODO: Check the missing intro an abstract tags on text.

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
 """Custom functions for document processing """
 
 __author__ = "Mati, Nico"
-__version__ = "0.1"
+__version__ = "0.2"
+
+import MySQLdb
 
 
 def document_wrapper(input_file):
@@ -29,39 +31,162 @@ def document_wrapper(input_file):
 """
 
 
-def insert_into_mysql(table, cols, rows):
-    """This function insert into mysql the required rows."""
+def get_abstract(input_file):
+    """This functions returns the abstract from a document file."""
 
-    import MySQLdb as mdb
+    abstracts = ["ABSTRACT", "Abstract"]
+    intros = ["INTRODUCTION", "Introduction"]
+    tmp_str = None
 
-    con = None
+    for a in abstracts:
+        if a in input_file:
+            tmp_str = input_file.split(a)[1]
 
-    try:
+            for i in intros:
+                if i in input_file:
+                    tmp_str = input_file.split(i)[0]
+                    return tmp_str
 
-        con = mdb.connect('localhost', 'mestrada',
-            '123456', 'inf335')
-        # Check if the connection has been stablished
-        if con:
-            cur = con.cursor()
+    print "[Abstract]: No intrdcution/abstract tags found."
+    return None
 
-            query = "INSERT INTO " + table + " ( " + ', '.join(cols) + ") VALUES ("
-            query += ", ".join(["%s" for i in range(0, len(cols))])
-            query += ")"
 
-            if len(rows) > 1:
-                cur.executemany(query, rows)
-            elif len(rows) == 1:
-                cur.execute(query, rows[0])
+def remove_punctuation(text):
+    """This function removes the punctuation in the text"""
+    from string import punctuation
+    exclude = set(punctuation)
+    text = ''.join(ch for ch in text if ch not in exclude)
+    return text
 
-            # print cur._last_executed
 
-            con.commit()
-        else:
-            print "Connection problem"
+def remove_stopwords(tokens, lang="english"):
+    """This function removes the stopwords."""
+    from nltk.corpus import stopwords
+    important_words = filter(lambda x: x not in stopwords.words(lang), tokens)
+    return important_words
 
-    except mdb.Error, e:
-        print "[MySql] Error ", e
 
-    finally:
-        if con:
-            con.close()
+def decode_text(text):
+    """This function decode the text."""
+    return text
+
+
+def tokenize(text):
+    """This function tokenize the text."""
+    from nltk import word_tokenize
+    return word_tokenize(text)
+
+
+def year_to_str(year):
+    """This function returns a string with the last two digits of a year"""
+    return str(year)[-2:]
+
+
+def get_str_id(id):
+    """This function return the id with corresponding leading zeros."""
+    return str(id).zfill(4)
+
+
+# Database class
+class mysql_db(object):
+    def __init__(self, host, port, dbname, username, password):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.dbname = dbname
+        self.db_connection = None
+        self.cursor = None
+        self.connecting = False
+
+    def __del__(self):
+        self.disconnect()
+
+    def connect(self):
+        """Call this function to connect to MySQL.
+        Output: Returns True when connected"""
+
+        self.connecting = True
+
+        try:
+            self.db_connection = MySQLdb.connect(
+                passwd=self.password,
+                db=self.dbname,
+                host=self.host,
+                port=self.port,
+                user=self.username,
+                charset="utf8",
+                use_unicode="True")
+            # Setting autocommit True for InnoDB transactions
+            self.db_connection.autocommit(True)
+            self.cursor = self.db_connection.cursor()
+            self.connecting = False
+
+        except MySQLdb.Error, e:
+            print ("DB Error {0}: {1}"
+                               .format(e.args[0], e.args[1]))
+            return False
+
+        except Exception, e:
+            print ("Error: {0}".format(str(e)))
+            return False
+        return True
+
+    def disconnect(self):
+        """Disconnect from MySQL"""
+        if self.db_connection:
+            self.db_connection.close()
+            self.db_connection = None
+
+    def insert_into_mysql(self, table, cols, rows):
+        """This function insert into mysql the required rows."""
+
+        self.connect()
+
+        try:
+            # Check if the connection has been stablished.
+            if self.db_connection:
+
+                query = "INSERT INTO " + table + " ( " + ', '.join(cols) + ") VALUES ("
+                query += ", ".join(["%s" for i in range(0, len(cols))])
+                query += ")"
+
+                if len(rows) > 1:
+                    self.cursor.executemany(query, rows)
+                elif len(rows) == 1:
+                    self.cursor.execute(query, rows[0])
+
+                # print self.cursor._last_executed
+
+                self.db_connection.commit()
+            else:
+                print "Connection problem"
+
+        except MySQLdb.Error, e:
+            print "[MySql] Error ", e
+
+        finally:
+            if self.db_connection:
+                self.disconnect()
+
+    def query(self, query):
+        """This function insert into mysql the required rows."""
+
+        self.connect()
+
+        try:
+            # Check if the connection has been stablished.
+            if self.db_connection:
+                self.cursor.execute(query)
+                result = self.cursor.fetchall()
+                return result
+
+            else:
+                print "Connection problem"
+
+        except MySQLdb.Error, e:
+            print "[MySql] Error ", e
+
+        finally:
+            if self.db_connection:
+                self.disconnect()
--- tarea1.py
+++ tarea1.py
@@ -7,8 +7,8 @@ __version__ = "0.1"
 
 #import MySQLdb
 
-from helpers import document_wrapper
-from helpers import insert_into_mysql
+from helpers import (document_wrapper, mysql_db, year_to_str, get_abstract,
+    get_str_id, remove_stopwords, tokenize, remove_punctuation)
 
 years = ["01", "02", "03", "04", "05", "06",
 "07", "08", "09", "10", "11", "12"]
@@ -18,19 +18,52 @@ years = ["01", "02", "03", "04", "05", "06",
 # title and autors)
 
 # idx folder relative path
-idx_path = "nipstxt/idx/"
-columns = ["ID", "title", "year", "authors"]
+base_path = "nipstxt/"
+idx_path = base_path + "idx/"
+doc_columns = ["ID", "title", "year", "authors"]
+abs_columns = ["ID", "year", "abstract"]
 
-for year in years:
-    rows = []
-    with open(idx_path + "c" + year + ".txt", "r") as fo:
-        for doc in document_wrapper(fo.read().split('\n')):
-            insert_into_mysql(
-                'Document',
-                columns,
-                [(int(doc[2]), doc[0], 2000 + int(year), doc[1])])
 
-            # print "Title: ", doc[0]
-            # print "Authors: ", doc[1]
-            # print "Year: 20" + year
-            # print "ID: " + doc[2]
+def fetch_docs():
+    for year in years:
+        with open(idx_path + "c" + year + ".txt", "r") as fo:
+            for doc in document_wrapper(fo.read().split('\n')):
+                db = mysql_db('localhost', 3306, 'inf335', 'mestrada', '123456')
+                db.insert_into_mysql(
+                    'Document',
+                    doc_columns,
+                    [(int(doc[2]), doc[0], 2000 + int(year), doc[1])])
+
+                # print "Title: ", doc[0]
+                # print "Authors: ", doc[1]
+                # print "Year: 20" + year
+                # print "ID: " + doc[2]
+
+
+def fetch_abstracts():
+    # Fists we need the Id and year of each document.
+    db = mysql_db('localhost', 3306, 'inf335', 'mestrada', '123456')
+    results = db.query("SELECT ID, year FROM Document ORDER BY year;")
+
+    for result in results:
+        did = result[0]  # Document ID.
+        y = result[1]  # Document Year.
+        fobj = open(base_path + "nips" + year_to_str(y)
+            + "/" + get_str_id(did) + ".txt", "r")
+
+        with fobj as fo:
+            print "[Abstract]: ", did, y
+            abstract = get_abstract(fo.read())
+
+        if abstract:
+            text = remove_punctuation(abstract.lower())
+            tokens = tokenize(text)
+            cls_abs = remove_stopwords(tokens)
+            db.insert_into_mysql(
+                'Abstract',
+                abs_columns,
+                [(did, y, " ".join(cls_abs))])
+
+if __name__ == "__main__":
+    # fetch_docs()
+    fetch_abstracts()
